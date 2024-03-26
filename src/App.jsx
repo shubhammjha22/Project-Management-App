08b710d78bec67ef662caee1ee2bf3188f87477a
shubhammjha22 <68507333+shubhammjha22@users.noreply.github.com>
2024-03-26 18:26:22 +0530

    Update App.jsx
    Added some dummy data in the Project

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,16 @@ import SelectedProject from './components/SelectedProject';
 function App() {
   const [projectState,setProjectedsState] = useState({
     selectedProjectId:undefined,
-    projects:[],
-    tasks: [],
+    projects:[{
+      title: 'Project Management Dashboard', description: ' A centralized dashboard for managing projects, tasks, and team collaboration.', dueDate: '2024-04-11', id: 0.8427817419065569
+    },{
+      title: 'Project 2 Bug Tracking System', description: 'An application to track and manage software bugs reported during development.', dueDate: '2024-05-09', id: 0.9316643512128271
+    },{
+      title: 'Project 3: Inventory Management System', description: ' A system for tracking and managing inventory in warehouses and stores.', dueDate: '2024-03-15', id: 0.6222035316388039
+    }],
+    tasks: [
+      {text: 'Integrate User Authentication', projectId: 0.8427817419065569, id: 0.47115179423406217},{text: 'Implement Task Lists', projectId: 0.8427817419065569, id: 0.7467004950551459},{text: 'Create Dashboard Layout:', projectId: 0.8427817419065569, id: 0.44697872575989406},{text: 'Implement Notification System', projectId: 0.9316643512128271, id: 0.4467685586400114},{text: 'Develop Bug Tracking Database', projectId: 0.9316643512128271, id: 0.3149499395366173},{text: 'Design Bug Report Form', projectId: 0.9316643512128271, id: 0.4253125672716829},{text: 'Implement Reporting Features', projectId: 0.6222035316388039, id: 0.661472845981379},{text: 'Integrate Barcode Scanning', projectId: 0.6222035316388039, id: 0.6336398501903207},{text: 'Develop User Interface', projectId: 0.6222035316388039, id: 0.38756363675882444},{text: 'Design Database Schema', projectId: 0.6222035316388039, id: 0.5625057199176604}
+    ],
   })
 
   function handleAddTask(text){
